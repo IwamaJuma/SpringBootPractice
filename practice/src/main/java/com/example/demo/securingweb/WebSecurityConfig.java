7bdcd40768a3b4039d65d260290c14d9667b89f4
'iwama' <'takedajuma@gmail.com'>
2025-02-05 10:52:02 +0900

    signin実装中

--- practice/src/main/java/com/example/demo/securingweb/WebSecurityConfig.java
+++ practice/src/main/java/com/example/demo/securingweb/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.demo.securingweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,9 +11,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.example.demo.service.UserDetailsServiceImpl;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+  
+  @Autowired
+  private UserDetailsServiceImpl userDetailsService;
   
   @Bean
   PasswordEncoder passwordEncoder() {
@@ -40,6 +47,11 @@ public class WebSecurityConfig {
       );
     
     return http.build();
+  }
+  
+  @Autowired
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
   }
 
 }
